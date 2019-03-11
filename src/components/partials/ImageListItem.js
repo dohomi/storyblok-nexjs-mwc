f4dfb931361089f49cd1c5bcfba723fd922d222b
Dominic Garms <djgarms@gmail.com>
2019-03-11 19:17:34 +0800

    enable list item click for storyblok

--- src/components/partials/ImageListItem.js
+++ src/components/partials/ImageListItem.js
@@ -1,16 +1,19 @@
 import Image from './ImageListItemImg'
+import SbEditable from 'storyblok-react'
 
 const ImageListItem = (props) => {
   return (
-    <li className="mdc-image-list__item"
-        key={props._uid}
-        style={props.style}
-        onClick={props.onImageClick}>
-      {Image(props)}
-      <div className="mdc-image-list__supporting">
-        <span className="mdc-image-list__label">{props.label}</span>
-      </div>
-    </li>
+    <SbEditable content={props}>
+      <li className="mdc-image-list__item"
+          key={props._uid}
+          style={props.style}
+          onClick={props.onImageClick}>
+        {Image(props)}
+        <div className="mdc-image-list__supporting">
+          <span className="mdc-image-list__label">{props.label}</span>
+        </div>
+      </li>
+    </SbEditable>
   )
 }
 
