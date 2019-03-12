bab8cc0d98fdf0b9be4b7683e8a695999f0306de
Dominic Garms <djgarms@gmail.com>
2019-03-12 15:21:27 +0800

    minor modifications to show hide slide navigations

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,18 +3,27 @@ import Components from 'components/index'
 import SwipeableViews from 'react-swipeable-views'
 import React, {useState} from 'react'
 import clsx from 'clsx'
+import {IconButton} from '@rmwc/icon-button'
 
 const Slider = (props) => {
   const [slide, setSlide] = useState(0)
 
   const content = props.content
   const body = content.body
+  const properties = content.property || []
   const styles = {}
   const paginationClasses = clsx(
     'carousel-indicators',
-    content.pagination_classes
+    {'d-none': properties.includes('hide_pagination')}
   )
-
+  const carouselPrevClasses = clsx(
+    'carousel-control-prev',
+    {'d-none': properties.includes('hide_arrows')}
+  )
+  const carouselNextClasses = clsx(
+    'carousel-control-next',
+    {'d-none': properties.includes('hide_arrows')}
+  )
   function handleChangeIndex (item) {
     setSlide(body.findIndex(i => i._uid === item._uid))
   }
@@ -27,7 +36,16 @@ const Slider = (props) => {
                         onChangeIndex={(i) => setSlide(i)}>
           {body.map(item => Components(item))}
         </SwipeableViews>
-
+        <a className={carouselPrevClasses}
+           role="button"
+           onClick={() => setSlide(slide === 0 ? body.length - 1 : slide - 1)}>
+          <IconButton icon="arrow_back_ios"/>
+        </a>
+        <a className={carouselNextClasses}
+           role="button"
+           onClick={() => setSlide(slide === body.length - 1 ? 0 : slide + 1)}>
+          <IconButton icon="arrow_forward_ios"/>
+        </a>
         <ol className={paginationClasses}>
           {body.map((item, i) => (
             <li className={`${slide === i ? 'active' : ''}`}
