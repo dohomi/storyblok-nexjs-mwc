c8eefe3812fe9e79cda1a931e1035715efbf6ab1
Dominic Garms <djgarms@gmail.com>
2019-03-20 17:30:07 +0800

    add variables for hubspot

--- src/components/HubspotForm.js
+++ src/components/HubspotForm.js
@@ -1,14 +1,11 @@
 import SbEditable from 'storyblok-react'
 import HubSpot from 'react-hubspot-form'
 
-import {useEffect} from 'react'
-
 const HubspotForm = ({content}) => {
   const opts = {
     portalId: content.portal_id,
     formId: content.form_id
   }
-
   return (
     <SbEditable content={content}>
       <HubSpot {...opts}
