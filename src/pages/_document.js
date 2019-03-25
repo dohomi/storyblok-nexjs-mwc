90ad053c9df23887def9fb6a263477abb8c080c0
Dominic Garms <djgarms@gmail.com>
2019-03-25 20:55:32 +0800

    add GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,20 @@ import Document, {Head, Main, NextScript} from 'next/document'
 import StoryblokService from '../utils/StoryblokService'
 import WebpService from '../utils/WebpService'
 import DeviceDetectService from '../utils/DeviceDetectService'
-import NextHead from '../components/layout/Head'
+
+function getGoogleTagManager () {
+  if (!process.env.GTM_CONTAINER || !process.env.NODE_ENV !== 'production') {
+    return null
+  }
+  return {
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-${process.env.GTM_CONTAINER}');`
+  }
+}
+
 
 class MyDocument extends Document {
   render () {
@@ -14,10 +27,23 @@ class MyDocument extends Document {
       var userDevice = ${JSON.stringify(DeviceDetectService.getDevice())};
       var hasWebpSupport = ${WebpService.getWebpSupport()};`
     }
+
+    const GTM = getGoogleTagManager()
+
     return (
       <html>
-      <Head/>
+      <Head>
+        {GTM && (
+          <script dangerouslySetInnerHTML={GTM}></script>
+        )}
+      </Head>
       <body className="mdc-typography mdc-theme--background">
+      {GTM && (
+        <noscript>
+          <iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_CONTAINER}`} height="0" width="0"
+                  style="display:none;visibility:hidden"></iframe>
+        </noscript>
+      )}
       <Main/>
       <script dangerouslySetInnerHTML={injectBodyScript}></script>
       <script crossOrigin="anonymous"
