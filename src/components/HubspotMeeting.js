0fc9d55838058e1117e08643a295046f3c4ec6b1
Dominic Garms <djgarms@gmail.com>
2019-04-01 10:04:52 +0800

    finalize meeting app

--- src/components/HubspotMeeting.js
+++ src/components/HubspotMeeting.js
@@ -1,20 +1,14 @@
 import SbEditable from 'storyblok-react'
-import {useState, useEffect} from 'react'
-
+import {useEffect} from 'react'
 
 const HubspotMeeting = ({content}) => {
-
-  const [isLoaded, setIsLoaded] = useState(false)
-
+  const dataSrc = `https://app.hubspot.com/meetings/${content.meeting_name}?embed=true`
   useEffect(
     () => {
       const script = document.createElement('script')
       script.src = 'https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js'
       script.async = true
       document.body.appendChild(script)
-      script.onload = () => {
-        setIsLoaded(true)
-      }
     },
     []
   )
@@ -22,11 +16,8 @@ const HubspotMeeting = ({content}) => {
   return (
     <SbEditable content={content}>
       <div className="lm-hubspot-meeting">
-        {isLoaded && (
-          <div className="meetings-iframe-container"
-               data-src="https://blog.commercecentric.com/meetings/phil132?embed=true"></div>
-
-        )}
+        <div className="meetings-iframe-container"
+             data-src={dataSrc}></div>
       </div>
     </SbEditable>
   )
