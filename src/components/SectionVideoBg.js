7dd22406efa23dd97a559f721b8d36c3870ffd85
Dominic Garms <djgarms@gmail.com>
2019-04-04 11:08:51 +0800

    finalize video component

--- src/components/SectionVideoBg.js
+++ src/components/SectionVideoBg.js
@@ -1,17 +1,26 @@
 import Components from 'components/index'
 import SbEditable from 'storyblok-react'
 import dynamic from 'next/dynamic'
-
+import {useInView} from 'react-intersection-observer'
+import {useEffect, useState} from 'react'
+import withWindowDimensions from './provider/WithWindowDimensions'
 
 const FullscreenVideoBg = dynamic(
   () => import('./partials/FullscreenVideoBg'),
   {ssr: false}
 )
 
-const SectionVideoBg = ({content}) => {
+const SectionVideoBg = ({content, dimensions}) => {
   const hasSrc = !!content.url
   const body = content.body || []
-  const hasBody = body.length
+  const hasBody = !!body.length
+  let fixedToRatio = !hasBody && !content.height
+  const [containerDimensions, setContainerDimensions] = useState({})
+  const [intersectionRef, inView, intersectionElement] = useInView({
+    triggerOnce: true,
+    rootMargin: '300px 0px 300px 0px'
+  })
+
   let ratioHeight = 9
   let ratioWidth = 16
   if (content.video_ratio) {
@@ -23,19 +32,33 @@ const SectionVideoBg = ({content}) => {
   const containerStyle = {}
   if (content.height) {
     containerStyle.minHeight = `${content.height}vh`
-  } else {
   }
-  // const containerStyleChildren = {
-  //   paddingTop: `${((ratioHeight / ratioWidth) * 100).toFixed(2)}%`
-  // }
+
+  useEffect(
+    () => {
+      if (inView) {
+        if (!fixedToRatio) {
+          const current = intersectionElement.target
+          setContainerDimensions({
+            width: current.clientWidth,
+            height: current.clientHeight
+          })
+        }
+      }
+    },
+    [inView, dimensions.width, dimensions.height, content.url]
+  )
 
 
   return (
     <SbEditable content={content}>
       <div className="lm-content-section lm-video-section"
-           style={containerStyle}>
-        {hasSrc && (
+           style={containerStyle}
+           ref={intersectionRef}>
+        {hasSrc && inView && (
           <FullscreenVideoBg {...content}
+                             containerDimensions={containerDimensions}
+                             fixedToRatio={fixedToRatio}
                              ratioHeight={ratioHeight}
                              ratioWidth={ratioWidth}/>
         )}
@@ -45,4 +68,4 @@ const SectionVideoBg = ({content}) => {
   )
 }
 
-export default SectionVideoBg
+export default withWindowDimensions(dimensions => ({dimensions}))(SectionVideoBg)
--- src/components/partials/FullscreenVideoBg.js
+++ src/components/partials/FullscreenVideoBg.js
@@ -2,36 +2,13 @@ import clsx from 'clsx'
 import ReactPlayer from 'react-player'
 import BackgroundImageContainer from './BackgroundImageContainer'
 import {useState} from 'react'
-import withWindowDimensions from '../provider/WithWindowDimensions'
 
 const FullscreenVideoBg = (content) => {
-  const windowWidth = content.dimensions.width
-  const windowHeight = content.dimensions.height
   const properties = content.property || []
-  const windowAspect = windowHeight / windowWidth
   const videoAspect = content.ratioHeight / content.ratioWidth
-  let vidBgWidth = '100%'
-  if (windowAspect > videoAspect) {
-
-    vidBgWidth = (windowAspect / videoAspect * 100).toFixed(2) + '%'
-  }
-
+  let fixedToRatio = content.fixedToRatio
   const [error, setError] = useState(false)
-  const contentHeight = content.height
-  const className = clsx('react-player', {
-    [`react-player__${contentHeight ? 'fit-into-ratio' : 'fixed-ratio'}`]: true
-  })
-  let style = {}
-  if (contentHeight) {
-    // style = {
-    //   width: '100vw',
-    //   minHeight: `${contentHeight}vw`,
-    //   height: `${((content.ratioHeight / content.ratioWidth) * 100).toFixed(2)}vw`, // '56.25   vw',
-    //   minWidth: `${((content.ratioWidth / content.ratioHeight) * 100).toFixed(2)}vw` // '177.77vh'
-    // }
-  }
-  let hasError
-
+  const className = clsx('react-player')
   if (!content.url) {
     return (
       <div>please insert a video URL</div>
@@ -46,11 +23,35 @@ const FullscreenVideoBg = (content) => {
     onError: () => setError(true)
   }
 
+  // render video based on video ratio
+  if (fixedToRatio) {
+    return (
+      <div style={{paddingBottom: `${(videoAspect * 100).toFixed(2)}%`, position: 'relative', width: '100%'}}>
+        <ReactPlayer url={content.url}
+                     className={className}
+                     width="100%"
+                     height="100%"
+                     {...playerProps}/>
+      </div>
+    )
+  }
+
+  // calculate video container to fit into available space
+  const windowWidth = content.containerDimensions.width
+  const windowHeight = content.containerDimensions.height
+  const windowAspect = windowHeight / windowWidth
+  let vidBgWidth = '100%'
+  if (windowAspect > videoAspect) {
+    vidBgWidth = (windowAspect / videoAspect * 100).toFixed(2) + '%'
+  }
+
+  // cover the available space
   return (
     <>
       <div className={`videobg-width${properties.includes('suppress_mouse_events') ? ' video-no-mouse' : ''}`}
            style={{width: vidBgWidth}}>
-        <div className="videobg-aspect">
+        <div className="videobg-aspect"
+             style={{paddingBottom: `${(videoAspect * 100).toFixed(2)}%`}}>
           <div className="videobg-make-height">
             <ReactPlayer url={content.url}
                          className={className}
@@ -65,4 +66,4 @@ const FullscreenVideoBg = (content) => {
   )
 }
 
-export default withWindowDimensions(dimensions => ({dimensions}))(FullscreenVideoBg)
+export default FullscreenVideoBg
