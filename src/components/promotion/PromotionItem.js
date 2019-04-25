9d9a3a17c7bd6e0f7f89b67219b1f46e5c93006b
Dominic Garms <djgarms@gmail.com>
2019-04-25 11:38:24 +0800

    further promotion animations

--- src/components/promotion/PromotionItem.js
+++ src/components/promotion/PromotionItem.js
@@ -9,30 +9,33 @@ const PromotionItem = (props) => {
   const body = props.body || []
   const action = props.action || []
   const className = clsx('lm-promotion__item', {
-    [`lm-promotion__item-${props.variant ? props.variant : 'variant'}`]: true
+    [`lm-promotion__item-${props.variant ? props.variant : 'variant'}`]: true,
+    [`lm-promotion__item-${props.position}`]: !!props.position
   })
 
-  useEffect(
-    () => {
-      const containerHeight = container.current.clientHeight + 1
-      const isMobile = props.dimensions.width <= 600
-      if (props.position === 'bottom_left_overlap' && isMobile) {
-        container.current.style.marginTop = '-25px'
-      } else if (props.position === 'bottom_left_overlap') {
-        container.current.style.marginTop = `-${containerHeight - 40}px`
-      }
-    },
-    [props.dimensions]
-  )
+  // useEffect(
+  //   () => {
+  //     const containerHeight = container.current.clientHeight + 1
+  //     const isMobile = props.dimensions.width <= 600
+  //     if (props.position === 'bottom_left_overlap' && isMobile) {
+  //       // container.current.style.marginTop = '-25px'
+  //     } else if (props.position === 'bottom_left_overlap') {
+  //       // container.current.style.marginTop = `-${containerHeight - 40}px`
+  //     }
+  //   },
+  //   [props.dimensions]
+  // )
 
   return (
     <SbEditable content={props}>
       <div className={className} ref={container}>
-        <div className="lm-promotion__content">
-          {body.map(blok => Components(blok))}
-        </div>
-        <div className="lm-promotion__action">
-          {action.map(blok => Components(blok))}
+        <div>
+          <div className="lm-promotion__content">
+            {body.map(blok => Components(blok))}
+          </div>
+          <div className="lm-promotion__action">
+            {action.map(blok => Components(blok))}
+          </div>
         </div>
       </div>
     </SbEditable>
