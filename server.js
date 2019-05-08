6d597b0fe94839e1787ea8265d395d1b3492ee58
Dominic Garms <djgarms@gmail.com>
2019-05-08 09:20:52 +0800

    remove focal point console

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const generateSitemap = require('./src/api/sitemap')
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
-// const handler = routes.getRequestHandler(app)
+const handler = routes.getRequestHandler(app)
 
-const handler = routes.getRequestHandler(app, ({req, res, route, query}) => {
-  if (req.url === '/sitemap.xml') {
-    return generateSitemap(req, res)
-  }
-  app.render(req, res, route.page, query)
-})
+// const handler = routes.getRequestHandler(app, ({req, res, route, query}) => {
+//   if (req.url === '/sitemap.xml') {
+//     return generateSitemap(req, res)
+//   }
+//   app.render(req, res, route.page, query)
+// })
 app.prepare()
   .then(() => {
     createServer(handler)
