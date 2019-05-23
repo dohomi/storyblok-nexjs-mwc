253146674dcf19ca4646d4fd937ef1cd7c58f610
Dominic Garms <djgarms@gmail.com>
2019-05-23 15:13:09 +0200

    add line-through and promo

--- src/components/pricing/PricingItem.js
+++ src/components/pricing/PricingItem.js
@@ -11,6 +11,7 @@ const PricingItem = (props) => {
   const media = props.image && props.image[0]
   const title = props.title || []
   const price = props.price || []
+  const promo = props.promo || []
   const features = props.features || []
   const subtitle = props.subtitle || []
   const button = props.button || []
@@ -19,11 +20,18 @@ const PricingItem = (props) => {
       <Card style={{height: '100%'}}>
         {media && <div className="lm-pricing__media">{Components(media)}</div>}
         <div className={`lm-pricing__title${!title.length ? ' lm-no-title' : ''}`}>{title.map(v => Components(v))}</div>
-        {price.length > 0 && <div className="lm-pricing__price">{price.map(v => Components(v))}</div>}
+        {price.length > 0 && (
+          <div className="lm-pricing__price">
+            {promo.length > 0 && promo.map(v => Components(v))}
+            {price.map(v => Components(v))}
+          </div>
+        )}
         {subtitle.length > 0 && <div className="lm-pricing__subtitle">{subtitle.map(v => Components(v))}</div>}
-        {features.length > 0 && <div className="lm-pricing__features"><ul>{features.map(v => {
-          return <li key={v._uid}>{Components(v)}</li>
-        })}</ul></div>}
+        {features.length > 0 && <div className="lm-pricing__features">
+          <ul>{features.map(v => {
+            return <li key={v._uid}>{Components(v)}</li>
+          })}</ul>
+        </div>}
         {button.length > 0 && <div className="lm-pricing__action">{button.map(v => Components(v))}</div>}
       </Card>
     </SbEditable>
