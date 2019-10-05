20be84599d245c1ed87c38e0e4ff464be0967099
Dominic Garms <djgarms@gmail.com>
2019-10-05 13:58:39 +0800

    generate typings on component level

--- generate-ts-schema.js
+++ generate-ts-schema.js
@@ -1,10 +1,77 @@
 const {compile} = require('json-schema-to-typescript')
 const fs = require('fs')
-const components = require('./components.57415.json')
+const ComponentsJson = require('./components.57415.json')
 
-compileFromFile('components.57415.json')
-  .then(ts => {
-    console.log(ts)
+let tsString = []
 
-    fs.writeFileSync('components.d.ts', ts)
+async function genTsSchema () {
+
+  for (const values of ComponentsJson.components) {
+    const obj = {}
+    obj.title = values.name
+    obj.type = 'object'
+    obj.properties = typeMapper(values.schema)
+    const requiredFields = []
+    Object.keys(values.schema).forEach(key => {
+      if (values.schema[key].required) {
+        requiredFields.push(key)
+      }
+    })
+    if (requiredFields.length) {
+      obj.required = requiredFields
+    }
+    try {
+      const ts = await compile(obj, values.name, {bannerComment: ''})
+      tsString.push(ts)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+}
+
+function typeMapper (schema = {}) {
+  const parsedObj = Object.keys(schema).reduce((obj, key) => {
+    const schemaElement = schema[key]
+    const schemaType = parseType(schemaElement.type)
+    if (!schemaType) {
+      return obj
+    }
+    obj[key] = {
+      type: schemaType
+    }
+    if (schemaElement.options) {
+      obj[key] = {
+        enum: schemaElement.options.map(item => item.value)
+      }
+    }
+    return obj
+  }, {})
+
+  return parsedObj
+}
+
+function parseType (type) {
+  switch (type) {
+    case 'text':
+      return 'string'
+    case 'bloks':
+      return 'array'
+    case 'option':
+      return 'string'
+    case 'options':
+      return 'array'
+    case 'number':
+      return 'number'
+    default:
+      return null
+  }
+}
+
+
+genTsSchema()
+  .then(() => {
+
+    console.log(tsString)
+    fs.writeFileSync('src/typings/generated/components-schema.d.ts', tsString.join('\n'))
   })
