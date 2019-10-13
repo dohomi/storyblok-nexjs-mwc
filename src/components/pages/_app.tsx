a57999a4dd1f056b82ade2da0c7a4af3db9b83bd
Dominic Garms <djgarms@gmail.com>
2019-10-13 14:41:18 +0800

    change _app for better import possibility

---
+++ src/components/pages/_app.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import App from 'next/app'
+
+class MyApp extends App {
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Component {...pageProps} />
+    )
+  }
+}
+
+// need to check if its still needed.. maybe move layout to this component!
+MyApp.getInitialProps = async ({Component, ctx}) => {
+  let pageProps = {}
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  return {pageProps}
+}
+
+export default MyApp
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,4 @@
-import React from 'react'
-import App from 'next/app'
+import MyApp from '../components/pages/_app'
 import '../assets/scss/Layout.scss'
-
-class MyApp extends App {
-
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <Component {...pageProps} />
-    )
-  }
-}
-
-// need to check if its still needed.. maybe move layout to this component!
-MyApp.getInitialProps = async ({Component, ctx}) => {
-  let pageProps = {}
-
-  if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx)
-  }
-
-  return {pageProps}
-}
 
 export default MyApp
