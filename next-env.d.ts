acc3ff2943d451f39b29e368884549ab12c98f77
Dominic Garms <djgarms@gmail.com>
2019-10-14 09:02:10 +0800

    modif next-env

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,13 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-interface RootComponentProps {
-  content: any
-}
-
-declare global {
-  interface Window {
-    userDevice: any
-    hasWebpSupport: boolean
-  }
-}
