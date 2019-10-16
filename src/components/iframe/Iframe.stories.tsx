1cdaa0479c6dfd3565e3b02c84e0e0978dfc56e3
Dominic Garms <djgarms@gmail.com>
2019-10-16 13:19:14 +0800

    add test

--- src/components/iframe/Iframe.stories.tsx
+++ src/components/iframe/Iframe.stories.tsx
@@ -1,13 +1,21 @@
 import { storiesOf } from '@storybook/react'
 import Iframe from './Iframe'
-import { IframeStoryblok } from '../../typings/generated/components-schema'
+import { IframeAdvancedStoryblok, IframeStoryblok } from '../../typings/generated/components-schema'
 import * as React from 'react'
+import IframeAdvanced from './IframeAdvanced'
 
 const props: IframeStoryblok = {
   _uid: '2313',
   component: 'iframe',
   url: 'https://www.youtube.com/embed/tgbNymZ7vqY'
 }
+
+const advanced: IframeAdvancedStoryblok = {
+  _uid: '1231',
+  component: 'iframe_advanced',
+  url: 'https://mysga.studentsgoabroad.com/?id=cj9sfuvq9onal0116182ztkb0'
+}
+
 storiesOf('Iframe', module)
   .add(
     'Iframe',
@@ -26,5 +34,11 @@ storiesOf('Iframe', module)
         <h3>4 by 3</h3>
         <Iframe content={{ ...props, responsive_ratio: '4by3' }} />
       </>
+    )
+  )
+  .add(
+    'Iframe Advanced',
+    () => (
+      <IframeAdvanced content={advanced}/>
     )
   )
