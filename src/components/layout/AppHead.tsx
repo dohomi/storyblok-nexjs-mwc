8fa99242163343d63871ecb05d5b3795df020cf1
Dominic Garms <djgarms@gmail.com>
2019-12-03 11:11:01 +0800

    preconnect to jsdelivr

--- src/components/layout/AppHead.tsx
+++ src/components/layout/AppHead.tsx
@@ -22,6 +22,7 @@ const AppHead: FunctionComponent<{ settings: GlobalStoryblok }> = ({ settings })
         <script src={`//app.storyblok.com/f/storyblok-latest.js?t=${StoryblokService.getToken()}`}></script>
       )}
       <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="anonymous" />
+      <link rel="preconnect" href="https://cdn.jsdelivr.net/" crossOrigin="anonymous" />
       <link href={`https://fonts.googleapis.com/css?family=${loadFonts.join('|')}&display=swap`} rel="stylesheet" />
     </NextHead>
   )
