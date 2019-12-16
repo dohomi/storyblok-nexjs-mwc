7ed651f3470746e4e45193d26d9e48f9086246fe
Dominic Garms <djgarms@gmail.com>
2019-12-16 15:31:25 +0800

    add find polyfill

--- client/polyfills.js
+++ client/polyfills.js
@@ -1,10 +1,12 @@
 import 'intersection-observer'
 import includes from 'core-js/library/fn/string/virtual/includes'
 import includes2 from 'core-js/library/fn/array/includes';
+import find from 'core-js/library/fn/array/find';
 import repeat from 'core-js/library/fn/string/virtual/repeat'
 import assign from 'core-js/library/fn/object/assign'
 // import 'unfetch/polyfill'
 Array.prototype.includes = includes2;
+Array.prototype.find = find;
 String.prototype.includes = includes
 String.prototype.repeat = repeat
 Object.assign = assign
