45b4aa3db5b37dc6313d2031fd8a2ec863df442d
Dominic Garms <djgarms@gmail.com>
2020-01-06 16:30:08 +0100

    minor change

--- src/components/timeline/TimelineRowItem.tsx
+++ src/components/timeline/TimelineRowItem.tsx
@@ -5,29 +5,26 @@ import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import { TimelineItemStoryblok } from '../../typings/generated/components-schema'
 import Components from '@components'
+import clsx from 'clsx'
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   cardContainer: {
     position: 'relative'
   },
-  cardDecoratorLeft: {
+  cardDecorator: {
     position: 'absolute',
     width: 0,
     height: 0,
     borderTop: '16px solid transparent',
-    borderLeft: '16px solid' + theme.palette.grey.A100,
     borderBottom: '16px solid transparent',
-    top: 'calc(50% - 16px)',
-    left: '100%'
+    top: 'calc(50% - 16px)'
+  },
+  cardDecoratorLeft: {
+    left: '100%',
+    borderLeft: '16px solid' + theme.palette.grey.A100
   },
   cardDecoratorRight: {
-    position: 'absolute',
-    width: 0,
-    height: 0,
-    borderTop: '16px solid transparent',
     borderRight: '16px solid' + theme.palette.grey.A100,
-    borderBottom: '16px solid transparent',
-    top: 'calc(50% - 16px)',
     right: '100%'
   }
 }))
@@ -40,8 +37,7 @@ const TimelineRowItem: FunctionComponent<{
   const body = content.body || []
   return (
     <div className={classes.cardContainer}>
-      <div className={isLeft ?
-        classes.cardDecoratorLeft : classes.cardDecoratorRight} />
+      <div className={clsx(classes.cardDecorator, isLeft ? classes.cardDecoratorLeft : classes.cardDecoratorRight)} />
       <Card>
         {(content.title || content.subheader) && <CardHeader title={content.title} subheader={content.subheader} />}
         {body.length > 0 && <CardContent>{body.map(blok => Components(blok))}</CardContent>}
