eff32b0746cb95cc0aa6bec1518116779884b40c
Dominic Garms <djgarms@gmail.com>
2020-03-10 17:10:16 +0800

    fix internal routing of nextjs

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import Header from './toolbar/Header'
 import Footer from './Footer'
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect } from 'react'
 import MwcDrawer from './drawer/MwcDrawer'
 import { AppPageProps } from '../../utils/parsePageProperties'
 import useAppScroll from '../../utils/hooks/useAppScroll'
 import AppHead from './AppHead'
 import useExternalScripts from '../../utils/hooks/useExternalScripts'
+import { useRouter } from 'next/router'
+import { closeNavigationDrawers } from '../../utils/state/actions'
 
 export type LayoutComponentProps = Pick<AppPageProps, 'settings'> & {
   hasFeature: boolean
@@ -17,6 +19,14 @@ const Layout: FunctionComponent<LayoutComponentProps> = ({ settings, children, h
 
   useAppScroll({ settings, hasFeature })
   useExternalScripts()
+  const router = useRouter()
+  const { asPath } = router
+  useEffect(
+    () => {
+      closeNavigationDrawers() // todo needs testing might need a pure close drawer action
+    },
+    [asPath]
+  )
 
   return (
     <>
--- src/pages/[...index].tsx
+++ src/pages/[...index].tsx
@@ -1,14 +1,12 @@
 import Components from '@components'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Layout from '../components/layout/Layout'
-import { useRouter } from 'next/router'
 import Error from '../pages/_error'
 import { NextPage } from 'next'
 import getInitialPageProps from '@initialData/getInitialPageProps'
 import { AppPageProps, PageSeoProps } from '../utils/parsePageProperties'
 import StoriesService from '../utils/StoriesService'
 import AppSeo from '../components/layout/AppSeo'
-import { closeNavigationDrawers } from '../utils/state/actions'
 import WindowDimensionsProvider from '../components/provider/WindowDimensionsProvider'
 import GlobalTheme from '../components/global-theme/GlobalTheme'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -16,14 +14,6 @@ import { useStoryblok } from '../utils/hooks/useStoryblok'
 
 const Index: NextPage<AppPageProps> = (props) => {
   const { settings, page, error, pageSeo } = useStoryblok(props)
-  const { asPath } = useRouter()
-
-  useEffect(
-    () => {
-      closeNavigationDrawers() // todo needs testing might need a pure close drawer action
-    },
-    [asPath]
-  )
 
   StoriesService.setAllStories(props.allStories)
   StoriesService.setAllCategories(props.allCategories)
@@ -46,18 +36,18 @@ const Index: NextPage<AppPageProps> = (props) => {
   }
 
   return (
-      <WindowDimensionsProvider>
-        <GlobalTheme settings={settings}>
-          <CssBaseline />
-          <AppSeo settings={settings} pageSeo={pageSeo as PageSeoProps} previewImage={page && page.preview_image} />
-          <Layout hasFeature={!!(page.property && page.property.includes('has_feature'))}
-                  settings={settings}
-                  hasRightDrawer={!!(page.right_body && page.right_body.length)}
-          >
-            {Components(page)}
-          </Layout>
-        </GlobalTheme>
-      </WindowDimensionsProvider>
+    <WindowDimensionsProvider>
+      <GlobalTheme settings={settings}>
+        <CssBaseline />
+        <AppSeo settings={settings} pageSeo={pageSeo as PageSeoProps} previewImage={page && page.preview_image} />
+        <Layout hasFeature={!!(page.property && page.property.includes('has_feature'))}
+                settings={settings}
+                hasRightDrawer={!!(page.right_body && page.right_body.length)}
+        >
+          {Components(page)}
+        </Layout>
+      </GlobalTheme>
+    </WindowDimensionsProvider>
   )
 }
 
