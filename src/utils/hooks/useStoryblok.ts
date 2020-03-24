b5200fb590b30551e60259697c7fcba6730c6c7f
Dominic Garms <djgarms@gmail.com>
2020-03-24 16:10:42 +0800

    prep previewData

--- src/utils/hooks/useStoryblok.ts
+++ src/utils/hooks/useStoryblok.ts
@@ -6,6 +6,10 @@ import StoryblokService from '../StoryblokService'
 export const useStoryblok = (props: AppPageProps) => {
   const { asPath, query } = useRouter()
   const insideStoryblok = !!query._storyblok
+  console.log('use storyblok', query)
+  if (typeof document !== 'undefined') {
+    console.log(document.location)
+  }
 
   let [content, setContent] = useState<AppPageProps>(props)
   useEffect(
