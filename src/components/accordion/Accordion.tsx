0b280735c7d313a54141812fb0be4f9048bc2a93
Dominic Garms <djgarms@gmail.com>
2020-05-06 22:29:27 +0800

    add minor options to accordion

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,13 +1,20 @@
 import SbEditable from 'storyblok-react'
 import AccordionItem from './AccordionItem'
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState } from 'react'
 import { AccordionItemStoryblok, AccordionStoryblok } from '../../typings/generated/components-schema'
 
 const Accordion: FunctionComponent<{ content: AccordionStoryblok }> = ({ content }) => {
+  const [opened, setOpen] = useState<string>('')
+
   return (
     <SbEditable content={content}>
       <div className="lm-accordion">
-        {(content.body || []).map((blok: AccordionItemStoryblok) => <AccordionItem {...blok} key={blok._uid} />)}
+        {(content.body || []).map((blok: AccordionItemStoryblok, iteration) => <AccordionItem content={blok}
+                                                                                              options={content}
+                                                                                              opened={opened}
+                                                                                              setOpen={setOpen}
+                                                                                              iteration={iteration}
+                                                                                              key={blok._uid} />)}
       </div>
     </SbEditable>
   )
