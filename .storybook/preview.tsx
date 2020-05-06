50b24b0b2793a6d196aa920c4df5ae78983567eb
Dominic Garms <djgarms@gmail.com>
2020-05-06 17:35:48 +0800

    sort storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,14 @@
-import { addDecorator } from '@storybook/react'
+import { addDecorator, addParameters } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
 import StoriesLayout from './components/StoriesLayout'
 import './mockNextRouter'
+
+addParameters({
+  options: {
+    storySort: (a: any, b: any) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
+  }
+})
 
 addDecorator(withKnobs)
 addDecorator(StoriesLayout)
