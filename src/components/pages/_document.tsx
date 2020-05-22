46474059a0c324d62ad5541e41d38b8db7ccf1bf
Dominic Garms <djgarms@gmail.com>
2020-05-22 11:08:17 +0800

    adjustments for _document

--- src/components/pages/_document.tsx
+++ src/components/pages/_document.tsx
@@ -2,10 +2,11 @@ import Document from 'next/document'
 import React from 'react'
 import { documentGetInitialProps, LmCoreDocument } from './CoreDocument'
 
-// for demo purpose only. we can't export class directly due to es5 error
-export default class MyDoc extends Document {
+// only works if a transpiled version is being used
+export class LmDefaultDocument extends Document {
   render() {
     return <LmCoreDocument props={this.props.__NEXT_DATA__.props} isDevelopment={this.props.isDevelopment} />
   }
 }
-MyDoc.getInitialProps = documentGetInitialProps
+
+LmDefaultDocument.getInitialProps = documentGetInitialProps
